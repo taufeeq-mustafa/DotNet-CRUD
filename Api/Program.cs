cfac24e783d7f4b8054e3b5a0d846744d4e51a1f
taufeeq <158837644+taufeeq-mustafa@users.noreply.github.com>
2024-10-31 17:39:16 +0500

    Update Program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -3,10 +3,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddDbContext<AppDbContext>(options=>options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -14,7 +14,7 @@ var app = builder.Build();
 
 app.MapControllers();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
